4e343eb6aa41665965d4f97c3ac617111ece4b04
Tom Hardman <tomhardman0@gmail.com>
2021-01-14 19:42:48 +0000

    images

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const OUTPUT_DIR = 'dist/static';
-
 const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
@@ -19,6 +19,11 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/static', to: '' }]
+    })
+  ],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, OUTPUT_DIR)
